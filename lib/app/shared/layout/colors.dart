3f5849d86134de67c969f494e9a4af8a50b57e1b
Juliano Tavares <julianojstavares@outlook.com>
2021-01-11 15:29:57 -0300

    Shared folder

---
+++ lib/app/shared/layout/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+ 
+class AppColors {
+  static var primary = const Color.fromRGBO(23, 49, 114, 1.0);
+  static var background = const Color.fromRGBO(245, 246, 248, 1.0);
+  static var textColor = const Color.fromRGBO(23, 49, 114, 1.0);
+  static var textColorBlack = Colors.black;
+  static var textColorWhite = Colors.white;
+  static Color secundary = Colors.white;
+}
