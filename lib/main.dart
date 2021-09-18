b2afa9819ac80ac02318b8e3b5f265c831793e95
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-09-18 13:30:32 -0300

    fix handler error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:piaui_app/app/app_module.dart';
 
 void main() async {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(ModularApp(module: AppModule()));
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
