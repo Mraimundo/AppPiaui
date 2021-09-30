074b8a86771467d63dfc1e81b45ffc0f4aaac10e
Mouzinho Raimundo <mouzinho@okn.com.br>
2021-09-30 11:03:27 -0300

    alteração no pubspec

--- lib/app/modules/inside_magazine/repository_teste/inside_repository.dart
+++ lib/app/modules/inside_magazine/repository_teste/inside_repository.dart
@@ -1,11 +1,8 @@
-import 'package:dio/dio.dart';
 import 'package:piaui_app/app/modules/inside_magazine/model/materia_model.dart';
-import 'package:piaui_app/app/shared/core/custom_dio.dart';
 
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<materiaModel> fetchMateria() async {
@@ -24,49 +21,3 @@ Future<materiaModel> fetchMateria() async {
     throw Exception('Failed to load album');
   }
 }
-
-// Future<materiaModel> getMateria({String id}) async {
-//   var dio = CustomDio().instance;
-
-//   materiaModel materia;
-//   try {
-//     Response materiaData =
-//         await dio.get('/customRest/v1/esquinasEdicao/?edicao=$id');
-
-//     print('Materia Info: $materiaData.data');
-//     materia = materiaModel.fromJson(materiaData.data);
-//   } on DioError catch (e) {
-//     if (e.response != null) {
-//       print("Dio Error");
-//       print("STATUS: ${e.response?.statusCode}");
-//       print("DATA: ${e.response?.data}");
-//     } else {
-//       print("Erro ao fazer a requisição!");
-//       print(e.message);
-//     }
-//   }
-//   return materia;
-// }
-
-// Future<materiaModel> getMateria({String id}) async {
-//   var dio = CustomDio().instance;
-
-//   materiaModel materia;
-//   try {
-//     Response materiaData =
-//         await dio.get('/customRest/v1/esquinasEdicao/?edicao=$id');
-
-//     print('Materia Info: $materiaData.data');
-//     materia = materiaModel.fromJson(materiaData.data);
-//   } on DioError catch (e) {
-//     if (e.response != null) {
-//       print("Dio Error");
-//       print("STATUS: ${e.response?.statusCode}");
-//       print("DATA: ${e.response?.data}");
-//     } else {
-//       print("Erro ao fazer a requisição!");
-//       print(e.message);
-//     }
-//   }
-//   return materia;
-// }
