b360cfe7064b7ad48d56fe187ad0ace933d3e376
Mouzinho Raimundo <mouzinho@okn.com.br>
2021-10-05 10:38:15 -0300

    ajustar o filtro das revistas

---
+++ lib/app/modules/all_editions_page/widgets/data_picker_mes.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:piaui_app/app/modules/all_editions_page/utils/utils.dart';
+
+class DataPickerMes extends StatefulWidget {
+  const DataPickerMes({Key key}) : super(key: key);
+
+  @override
+  _DataPickerMesState createState() => _DataPickerMesState();
+}
+
+class _DataPickerMesState extends State<DataPickerMes> {
+  int index = 0;
+  static List<String> values = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ];
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            buildCustomPicker(),
+          ],
+        ),
+      );
+  Widget buildCustomPicker() => SizedBox(
+        height: 260,
+        child: CupertinoPicker(
+          itemExtent: 40,
+          // looping: true,
+          onSelectedItemChanged: (index) => setState(() => this.index = index),
+          children: Utils.modelBuilder<String>(
+            values,
+            (index, value) {
+              return Center(
+                child: Text(value, style: TextStyle(fontSize: 14)),
+              );
+            },
+          ),
+        ),
+      );
+}
