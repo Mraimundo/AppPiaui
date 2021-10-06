9b11054683d107937bbe64b014d6ca16cb4408a0
MFraimundo <raimundo.mouzinho@teamsys.com.br>
2021-10-06 14:19:32 -0300

    alterar o tamanho do botão de entrar.

--- lib/app/shared/components/app_bar/login/widgets/login_widget.dart
+++ lib/app/shared/components/app_bar/login/widgets/login_widget.dart
@@ -73,6 +73,7 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     double vHeight = MediaQuery.of(context).size.height;
     double vWidth = MediaQuery.of(context).size.width;
     return Material(
@@ -160,13 +161,11 @@ class _LoginWidgetState extends State<LoginWidget> {
                 height: 13,
               ),
               Container(
-                margin: EdgeInsets.only(left: 0),
                 child: TextButton(
-                  // onPressed: () {},
                   child: Container(
                     color: AppColors.orangePiaui,
-                    height: vHeight * 0.06,
-                    width: vWidth / 2.5,
+                    height: size.height * 0.06,
+                    width: size.width / 2.5,
                     child: Align(
                       child: Text(
                         'Entrar',
@@ -178,7 +177,6 @@ class _LoginWidgetState extends State<LoginWidget> {
                     ),
                   ),
                   onPressed: () => _onPressed(),
-                  // loading: _isLoading,
                 ),
               ),
             ],
