11c5073acd913b9c80c5f8e4fd3286ea98d99888
LucasCampanuchi <lucasccorrea09@gmail.com>
2021-10-07 11:15:29 -0300

    Alterado endereço de prod

--- lib/app/shared/components/app_bar/login/api/api.dart
+++ lib/app/shared/components/app_bar/login/api/api.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
 
-BaseOptions options = BaseOptions(baseUrl: "https://piaui.homolog.inf.br/");
+BaseOptions options = BaseOptions(baseUrl: "https://piaui.folha.uol.com.br/");
 
 Dio api = Dio(options);
--- lib/app/shared/utils/constants.dart
+++ lib/app/shared/utils/constants.dart
@@ -1,1 +1,1 @@
-const String URL_BASE = 'https://piaui.homolog.inf.br/';
+const String URL_BASE = 'https://piaui.folha.uol.com.br/';
